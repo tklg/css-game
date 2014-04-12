ed40b9376c103427016fea5ccb95bb7b10eea472
Theodore Kluge <villa7.kluge5@gmail.com>
2014-04-12 16:01:36 -0400

    Things kind of work now

--- js/css.js
+++ js/css.js
@@ -1,56 +1,31 @@
 var csschoice_border_style = ["solid","dotted","dashed","double","groove","ridge","inset","outset"];
-// var csschoice_border_radius = [];
-// var csschoice_background_color = [];
-//var csschoice_color = [];
-// var csschoice_font_size = [];
-// var csschoice_text_decoration = ["none","underline","overline","line-through"];
-// var csschoice_text_align = ["left","center","right"];
-// var csschoice_margin = ["auto"]; /*This may screw up the position styling...*/
-// var csschoice_float = ["left","right"]
-// var csschoice_padding = [];
-// var csschoice_cursor=["alias","all-scroll","cell","col-resize","copy","crosshair","e-resize","help","n-resize","not-allowed","none","pointer","progress","row-resize","text","vertical-text","wait"];
-// var csschoice_height = [];
-// var csschoice_width = [];
-// var csschoice_letterspacing = [];
+var csschoice_background_color = [];
+var csschoice_color = [];
+var csschoice_text_decoration = ["none","underline","overline","line-through"];
+var csschoice_text_align = ["left","center","right"];
+var csschoice_float = ["left","right"]
+var csschoice_cursor=["alias","all-scroll","cell","col-resize","copy","crosshair","e-resize","help","n-resize","not-allowed","none","pointer","progress","row-resize","text","vertical-text","wait"];
+var csschoice_size = [];
+var csschoice_dims = [];
+var csschoice_margin = [];
 
-$(document).ready(function() {
-
-color(255);
-radius(50)
-margin(200);
-heightsize(400);
-space(30);
-
-initGame();
-
-});
+var temp_1, temp_2, temp_3, temp_4;
 
 var rand = Math.random();
+var hasStarted;
+
 
 color = function(maxVal) {
 	var tempArray = [];
-	for (i = 0; i < 40; i++) {
-		for (i = 0; i < 3; i++) {
-			tempArray[i] = Math.floor((rand * maxVal) + 1).toString();
-			i++;
-		}
-		var rgbColor = "rgb(" + tempArray[1] + "," + tempArray[2] + "," + tempArray[3] + ")";
-		if (i <= 20) {
-		  	csschoice_color[i] = rgbColor;
-	  	} else {
-	  		csschoice_background_color[i - 20] = rgbColor;
-	  	}
-	}
-}
-
-radius = function(maxVal) {
-
 	for (i = 0; i < 20; i++) {
+		for (v = 0; v < 3; v++) {
+			tempArray[v] = Math.round(Math.random() * maxVal) + 1;
+		}
+		var rgbColor = "rgb(" + tempArray[0] + "," + tempArray[1] + "," + tempArray[2] + ")";
 
-		var a = (Math.round(Math.floor((rand * maxVal) + 1) / 5) * 5);
+		 csschoice_color[i] = rgbColor;
+		 //console.log(i + " - " + rgbColor);
 
-		var borderRad = a.toString() + "px";
-	  	csschoice_border_radius[i] = borderRad;
 	}
 }
 
@@ -58,57 +33,63 @@ size = function(maxVal) {
 
 	for (i = 0; i < 20; i++) {
 
-		var a = (Math.round(Math.floor((rand * maxVal) + 1) / 5) * 5);
+		temp_1 = Math.round(Math.random() * maxVal) + 1;
 
-		var size = a.toString() + "px";
-	  	csschoice_border_radius[i] = size;
+		var borderRad = temp_1.toString() + "px";
+	  	csschoice_size[i] = borderRad;
+	  	//console.warn(i);
+	  	//console.warn(borderRad);
+	}
+}
+
+dims = function(maxVal) {
+
+	for (i = 0; i < 20; i++) {
+
+		temp_2 = Math.round(Math.random() * maxVal) + 1;
+
+		var dm = temp_2.toString() + "px";
+	  	csschoice_dims[i] = dm;
 	}
 }
 
 margin = function(maxVal) {
 
-	for (i = 2; i < 40; i++) {
-
-		var a = (Math.round(Math.floor((rand * maxVal) + 1) / 5) * 5);
-
-		if (i > 0 && i <= 20) {
-			var mg = a.toString() + "px";
-	  		csschoice_margin[i] = mg;
-	  	} else {
-	  		var pd = a.toString() + "px";
-	  		csschoice_padding[i - 20] = pd;
-	  	}
-	}
-}
-
-heightsize = function(maxVal) {
-
-	for (i = 20; i < 60; i++) {
-
-		var a = (Math.round(Math.floor((rand * maxVal) + 1) / 5) * 5);
-		
-		if (i <= 40) {
-			var h = a.toString() + "px";
-	  		csschoice_height[i] = h;
-	  	} else {
-	  		var w = a.toString() + "px";
-	  		csschoice_width[i - 20] = w;
-	  	}
-	}
-}
-
-space = function(maxVal) {
-
 	for (i = 0; i < 20; i++) {
 
-		var a = (Math.round(Math.floor((rand * maxVal) + 1) / 5) * 5);
+		//var a = ((Math.floor((Math.round(Math.Math.random()om())) * maxVal) + 1) / 5) * 5;
+		temp_3 = Math.round(Math.random() * maxVal) + 1;
+		//console.log(Math.round(Math.random() * maxVal) + 1);
+		//console.log("a = " + a);
+		//Math.floor((rr * maxVal) + 1) / 5)
 
-		var sp = a.toString() + "px";
-	  	csschoice_letterspacing[i] = sp;
+		var mg = temp_3.toString() + "px";
+	  	csschoice_margin[i] = mg;
 	}
+}
+
+getVars = function() {
+	color(255);
+	size(30);
+	dims(250);
+	margin(100);
 }
 
 /*
 csschoice_letterspacing.length
 csschoice_padding.indexOf(3);
 */
+$(document).ready(function() {
+
+hasStarted = false;
+startGame = function() {
+	if (!hasStarted) {
+		$('.css-box-title').css('display','none');
+		$('#goal').css('display','inline');
+		$('.footer').css('visibility','visible');
+		getVars();
+		initGame();
+	}
+}
+
+});
--- js/main.js
+++ js/main.js
@@ -1,37 +1,143 @@
-initGame = function() {
-    
-buildGoal();
+var correctAnswer;
+var csschoice_cursor=["alias","all-scroll","cell","col-resize","copy","crosshair","e-resize","help","n-resize","not-allowed","none","pointer","progress","row-resize","text","vertical-text","wait"];
+var goalText, goalCSS, goalTextWrong;
+var bs, br, bc, co, fs, td, ta, m, f, s, cu, d;
+var score = 0
+var wrong = 0
+var right = 0;
 
-var correctAnswer = Math.round(Math.random * 4);
-    if (correctAnswer != 1 || correctAnswer != 2 || correctAnswer != 3 || correctAnswer != 4) {
-        //correctAnswer = Math.round(Math.random * 4);
-    };
+var totalQuestions = 10;
 
-setGoal();
-setExampleContent();
-};
+//var rand = Math.random();
+//var n = rand;
 
 buildGoal = function() {
 
-var rand = Math.random();
+//Teh Answers
+bs = csschoice_border_style[Math.round(Math.random() * 7)];
+br = csschoice_size[Math.round(Math.random() * 19)];
+bc = csschoice_color[Math.round(Math.random() * 19)];
+co = csschoice_color[Math.round(Math.random() * 19)];
+fs = csschoice_size[Math.round(Math.random() * 19)];
+td = csschoice_text_decoration[Math.round(Math.random() * 3)];
+ta = csschoice_text_align[Math.round(Math.random() * 2)];
+m = csschoice_margin[Math.round(Math.random() * 19)];
+f = csschoice_float[Math.round(Math.random() * 1)];
+h = csschoice_size[Math.round(Math.random() * 19)];
+w = csschoice_size[Math.round(Math.random() * 19)];
+s = csschoice_size[Math.round(Math.random() * 19)];
+cu = csschoice_cursor[Math.round(Math.random() * 16)];
+d = csschoice_dims[Math.round(Math.random() * 19)];
 
-var goalText = "border-style: " + csschoice_border_style[rand * 8] + ";<br>border-radius: " + csschoice_border_radius[rand * 20] + ";<br>background-color: " + csschoice_background_color[rand * 20] + ";<br>color: " + csschoice_color[rand * 20] + ";<br>font-size: " + csschoice_font_size[rand * 20] + ";<br>text-decoration: " + csschoice_text_decoration[rand * 4] + ";<br>text-align: " + csschoice_text_align[rand * 3] + ";<br>margin: " + csschoice_margin[rand * 20] + ";<br>float: " + csschoice_float[rand * 2] + ";<br>padding: " + csschoice_padding[rand * 20] + ";<br>cursor: " + csschoice_cursor[Math.randon * 17] + ";<br>height: " + csschoice_height[rand * 20] + ";<br>width: " + csschoice_width[rand * 20] + ";<br>letter-spacing: " + csschoice_letterspacing[rand * 20] + ";";
-//var goalTextWrong = "border-style: " + csschoice_border_style[rand * 8] + ";<br>border-radius: " + csschoice_border_radius[rand * 20] + ";<br>background-color: " + csschoice_background_color[rand * 20] + ";<br>color: " + csschoice_color[rand * 20] + ";<br>font-size: " + csschoice_font_size[rand * 20] + ";<br>text-decoration: " + csschoice_text_decoration[rand * 4] + ";<br>text-align: " + csschoice_text_align[rand * 3] + ";<br>margin: " + csschoice_margin[rand * 20] + ";<br>float: " + csschoice_float[rand * 2] + ";<br>padding: " + csschoice_padding[rand * 20] + ";<br>cursor: " + csschoice_cursor[Math.randon * 17] + ";<br>height: " + csschoice_height[rand * 20] + ";<br>width: " + csschoice_width[rand * 20] + ";<br>letter-spacing: " + csschoice_letterspacing[rand * 20] + ";";
-//var goalCSS = "border-style":csschoice_border_style[rand * 8],"border-radius":csschoice_border_radius[rand * 20],"background-color":csschoice_background_color[rand * 20],"color":csschoice_color[rand * 20],"font-size":csschoice_font_size[rand * 20],"text-decoration":csschoice_text_decoration[rand * 4],"text-align":csschoice_text_align[rand * 3],"margin":csschoice_margin[rand * 20],"float":csschoice_float[rand * 2],"padding":csschoice_padding[rand * 20],"cursor":csschoice_cursor[Math.randon * 17],"height":csschoice_height[rand * 20],"width":csschoice_width[rand * 20],"letter-spacing":csschoice_letterspacing[rand * 20];
+correctAnswer = Math.round(Math.random() * 3);
 
+//console.log(bs, br, bc, co, fs, td, ta, m, f, s, cu, d);
+goalText = "border-style: " + bs + ";<br>border-radius: " + br + ";<br>background-color: " + bc + ";<br>color: " + co + ";<br>font-size: " + fs + ";<br>text-decoration: " + td + ";<br>text-align: " + ta + ";<br>margin: " + m + ";<br>float: " + f + ";<br>padding: " + m + ";<br>cursor: " + cu + ";<br>letter-spacing: " + s + ";";
+//goalTextWrong = "border-style: " + csschoice_border_style[rand * 7] + ";<br>border-radius: " + csschoice_border_radius[rand * 19 + ";<br>background-color: " + csschoice_background_color[rand * 19] + ";<br>color: " + csschoice_color[rand * 19] + ";<br>font-size: " + csschoice_font_size[rand * 19] + ";<br>text-decoration: " + csschoice_text_decoration[rand * 3] + ";<br>text-align: " + csschoice_text_align[rand * 2] + ";<br>margin: " + csschoice_margin[rand * 19] + ";<br>float: " + csschoice_float[rand * 1] + ";<br>padding: " + csschoice_padding[rand * 19] + ";<br>cursor: " + csschoice_cursor[Math.randon * 16] + ";<br>height: " + csschoice_height[rand * 19] + ";<br>width: " + csschoice_width[rand * 19] + ";<br>letter-spacing: " + csschoice_letterspacing[rand * 19] + ";";
 };
 
 setGoal = function() {
-    //$('#goal').css(goalCSS);
+    $('#goal').css({
+        "border-style":bs,
+        "border-radius":br,
+        "background-color":bc,
+        "color":co,
+        "font-size":fs,
+        "text-decoration":td,
+        "text-align":ta,
+        "margin":m,
+        "float":f,
+        "padding":m,
+        "cursor":cu,
+        "letter-spacing":s
+    });
 };
 
 setExampleContent = function() {
+    console.log("Correct box loc: " + correctAnswer);
 
-if (correctAnswer === 1) {
-    $('#ec1').html(goalText.toString());
-    $('#ec2').html("");
-    $('#ec3').html("");
-    $('#ec4').html("");
+switch (correctAnswer) {
+
+    case 0:
+        $('#1').html(goalText);
+        $('#2').html("border-style: " + csschoice_border_style[Math.round(Math.random() * 7)] + ";<br>border-radius: " + csschoice_size[Math.round(Math.random() * 19)] + ";<br>background-color: " + csschoice_color[Math.round(Math.random() * 19)] + ";<br>color: " + csschoice_color[Math.round(Math.random() * 19)] + ";<br>font-size: " + csschoice_size[Math.round(Math.random() * 19)] + ";<br>text-decoration: " + csschoice_text_decoration[Math.round(Math.random() * 3)] + ";<br>text-align: " + csschoice_text_align[Math.round(Math.random() * 2)] + ";<br>margin: " + csschoice_margin[Math.round(Math.random() * 19)] + ";<br>float: " + csschoice_float[Math.round(Math.random() * 1)] + ";<br>padding: " + csschoice_margin[Math.round(Math.random() * 19)] + ";<br>cursor: " + csschoice_cursor[Math.round(Math.random() * 16)] + ";<br>letter-spacing: " + csschoice_size[Math.round(Math.random() * 19)] + ";");
+        $('#3').html("border-style: " + csschoice_border_style[Math.round(Math.random() * 7)] + ";<br>border-radius: " + csschoice_size[Math.round(Math.random() * 19)] + ";<br>background-color: " + csschoice_color[Math.round(Math.random() * 19)] + ";<br>color: " + csschoice_color[Math.round(Math.random() * 19)] + ";<br>font-size: " + csschoice_size[Math.round(Math.random() * 19)] + ";<br>text-decoration: " + csschoice_text_decoration[Math.round(Math.random() * 3)] + ";<br>text-align: " + csschoice_text_align[Math.round(Math.random() * 2)] + ";<br>margin: " + csschoice_margin[Math.round(Math.random() * 19)] + ";<br>float: " + csschoice_float[Math.round(Math.random() * 1)] + ";<br>padding: " + csschoice_margin[Math.round(Math.random() * 19)] + ";<br>cursor: " + csschoice_cursor[Math.round(Math.random() * 16)] + ";<br>letter-spacing: " + csschoice_size[Math.round(Math.random() * 19)] + ";");
+        $('#4').html("border-style: " + csschoice_border_style[Math.round(Math.random() * 7)] + ";<br>border-radius: " + csschoice_size[Math.round(Math.random() * 19)] + ";<br>background-color: " + csschoice_color[Math.round(Math.random() * 19)] + ";<br>color: " + csschoice_color[Math.round(Math.random() * 19)] + ";<br>font-size: " + csschoice_size[Math.round(Math.random() * 19)] + ";<br>text-decoration: " + csschoice_text_decoration[Math.round(Math.random() * 3)] + ";<br>text-align: " + csschoice_text_align[Math.round(Math.random() * 2)] + ";<br>margin: " + csschoice_margin[Math.round(Math.random() * 19)] + ";<br>float: " + csschoice_float[Math.round(Math.random() * 1)] + ";<br>padding: " + csschoice_margin[Math.round(Math.random() * 19)] + ";<br>cursor: " + csschoice_cursor[Math.round(Math.random() * 16)] + ";<br>letter-spacing: " + csschoice_size[Math.round(Math.random() * 19)] + ";");
+        break;
+    case 1:
+        $('#1').html("border-style: " + csschoice_border_style[Math.round(Math.random() * 7)] + ";<br>border-radius: " + csschoice_size[Math.round(Math.random() * 19)] + ";<br>background-color: " + csschoice_color[Math.round(Math.random() * 19)] + ";<br>color: " + csschoice_color[Math.round(Math.random() * 19)] + ";<br>font-size: " + csschoice_size[Math.round(Math.random() * 19)] + ";<br>text-decoration: " + csschoice_text_decoration[Math.round(Math.random() * 3)] + ";<br>text-align: " + csschoice_text_align[Math.round(Math.random() * 2)] + ";<br>margin: " + csschoice_margin[Math.round(Math.random() * 19)] + ";<br>float: " + csschoice_float[Math.round(Math.random() * 1)] + ";<br>padding: " + csschoice_margin[Math.round(Math.random() * 19)] + ";<br>cursor: " + csschoice_cursor[Math.round(Math.random() * 16)] + ";<br>letter-spacing: " + csschoice_size[Math.round(Math.random() * 19)] + ";");
+        $('#2').html(goalText);
+        $('#3').html("border-style: " + csschoice_border_style[Math.round(Math.random() * 7)] + ";<br>border-radius: " + csschoice_size[Math.round(Math.random() * 19)] + ";<br>background-color: " + csschoice_color[Math.round(Math.random() * 19)] + ";<br>color: " + csschoice_color[Math.round(Math.random() * 19)] + ";<br>font-size: " + csschoice_size[Math.round(Math.random() * 19)] + ";<br>text-decoration: " + csschoice_text_decoration[Math.round(Math.random() * 3)] + ";<br>text-align: " + csschoice_text_align[Math.round(Math.random() * 2)] + ";<br>margin: " + csschoice_margin[Math.round(Math.random() * 19)] + ";<br>float: " + csschoice_float[Math.round(Math.random() * 1)] + ";<br>padding: " + csschoice_margin[Math.round(Math.random() * 19)] + ";<br>cursor: " + csschoice_cursor[Math.round(Math.random() * 16)] + ";<br>letter-spacing: " + csschoice_size[Math.round(Math.random() * 19)] + ";");
+        $('#4').html("border-style: " + csschoice_border_style[Math.round(Math.random() * 7)] + ";<br>border-radius: " + csschoice_size[Math.round(Math.random() * 19)] + ";<br>background-color: " + csschoice_color[Math.round(Math.random() * 19)] + ";<br>color: " + csschoice_color[Math.round(Math.random() * 19)] + ";<br>font-size: " + csschoice_size[Math.round(Math.random() * 19)] + ";<br>text-decoration: " + csschoice_text_decoration[Math.round(Math.random() * 3)] + ";<br>text-align: " + csschoice_text_align[Math.round(Math.random() * 2)] + ";<br>margin: " + csschoice_margin[Math.round(Math.random() * 19)] + ";<br>float: " + csschoice_float[Math.round(Math.random() * 1)] + ";<br>padding: " + csschoice_margin[Math.round(Math.random() * 19)] + ";<br>cursor: " + csschoice_cursor[Math.round(Math.random() * 16)] + ";<br>letter-spacing: " + csschoice_size[Math.round(Math.random() * 19)] + ";");
+        break;
+    case 2:
+        $('#1').html("border-style: " + csschoice_border_style[Math.round(Math.random() * 7)] + ";<br>border-radius: " + csschoice_size[Math.round(Math.random() * 19)] + ";<br>background-color: " + csschoice_color[Math.round(Math.random() * 19)] + ";<br>color: " + csschoice_color[Math.round(Math.random() * 19)] + ";<br>font-size: " + csschoice_size[Math.round(Math.random() * 19)] + ";<br>text-decoration: " + csschoice_text_decoration[Math.round(Math.random() * 3)] + ";<br>text-align: " + csschoice_text_align[Math.round(Math.random() * 2)] + ";<br>margin: " + csschoice_margin[Math.round(Math.random() * 19)] + ";<br>float: " + csschoice_float[Math.round(Math.random() * 1)] + ";<br>padding: " + csschoice_margin[Math.round(Math.random() * 19)] + ";<br>cursor: " + csschoice_cursor[Math.round(Math.random() * 16)] + ";<br>letter-spacing: " + csschoice_size[Math.round(Math.random() * 19)] + ";");
+        $('#2').html("border-style: " + csschoice_border_style[Math.round(Math.random() * 7)] + ";<br>border-radius: " + csschoice_size[Math.round(Math.random() * 19)] + ";<br>background-color: " + csschoice_color[Math.round(Math.random() * 19)] + ";<br>color: " + csschoice_color[Math.round(Math.random() * 19)] + ";<br>font-size: " + csschoice_size[Math.round(Math.random() * 19)] + ";<br>text-decoration: " + csschoice_text_decoration[Math.round(Math.random() * 3)] + ";<br>text-align: " + csschoice_text_align[Math.round(Math.random() * 2)] + ";<br>margin: " + csschoice_margin[Math.round(Math.random() * 19)] + ";<br>float: " + csschoice_float[Math.round(Math.random() * 1)] + ";<br>padding: " + csschoice_margin[Math.round(Math.random() * 19)] + ";<br>cursor: " + csschoice_cursor[Math.round(Math.random() * 16)] + ";<br>letter-spacing: " + csschoice_size[Math.round(Math.random() * 19)] + ";");
+        $('#3').html(goalText);
+        $('#4').html("border-style: " + csschoice_border_style[Math.round(Math.random() * 7)] + ";<br>border-radius: " + csschoice_size[Math.round(Math.random() * 19)] + ";<br>background-color: " + csschoice_color[Math.round(Math.random() * 19)] + ";<br>color: " + csschoice_color[Math.round(Math.random() * 19)] + ";<br>font-size: " + csschoice_size[Math.round(Math.random() * 19)] + ";<br>text-decoration: " + csschoice_text_decoration[Math.round(Math.random() * 3)] + ";<br>text-align: " + csschoice_text_align[Math.round(Math.random() * 2)] + ";<br>margin: " + csschoice_margin[Math.round(Math.random() * 19)] + ";<br>float: " + csschoice_float[Math.round(Math.random() * 1)] + ";<br>padding: " + csschoice_margin[Math.round(Math.random() * 19)] + ";<br>cursor: " + csschoice_cursor[Math.round(Math.random() * 16)] + ";<br>letter-spacing: " + csschoice_size[Math.round(Math.random() * 19)] + ";");
+        break;
+    case 3:
+        $('#1').html("border-style: " + csschoice_border_style[Math.round(Math.random() * 7)] + ";<br>border-radius: " + csschoice_size[Math.round(Math.random() * 19)] + ";<br>background-color: " + csschoice_color[Math.round(Math.random() * 19)] + ";<br>color: " + csschoice_color[Math.round(Math.random() * 19)] + ";<br>font-size: " + csschoice_size[Math.round(Math.random() * 19)] + ";<br>text-decoration: " + csschoice_text_decoration[Math.round(Math.random() * 3)] + ";<br>text-align: " + csschoice_text_align[Math.round(Math.random() * 2)] + ";<br>margin: " + csschoice_margin[Math.round(Math.random() * 19)] + ";<br>float: " + csschoice_float[Math.round(Math.random() * 1)] + ";<br>padding: " + csschoice_margin[Math.round(Math.random() * 19)] + ";<br>cursor: " + csschoice_cursor[Math.round(Math.random() * 16)] + ";<br>letter-spacing: " + csschoice_size[Math.round(Math.random() * 19)] + ";");
+        $('#2').html("border-style: " + csschoice_border_style[Math.round(Math.random() * 7)] + ";<br>border-radius: " + csschoice_size[Math.round(Math.random() * 19)] + ";<br>background-color: " + csschoice_color[Math.round(Math.random() * 19)] + ";<br>color: " + csschoice_color[Math.round(Math.random() * 19)] + ";<br>font-size: " + csschoice_size[Math.round(Math.random() * 19)] + ";<br>text-decoration: " + csschoice_text_decoration[Math.round(Math.random() * 3)] + ";<br>text-align: " + csschoice_text_align[Math.round(Math.random() * 2)] + ";<br>margin: " + csschoice_margin[Math.round(Math.random() * 19)] + ";<br>float: " + csschoice_float[Math.round(Math.random() * 1)] + ";<br>padding: " + csschoice_margin[Math.round(Math.random() * 19)] + ";<br>cursor: " + csschoice_cursor[Math.round(Math.random() * 16)] + ";<br>letter-spacing: " + csschoice_size[Math.round(Math.random() * 19)] + ";");
+        $('#3').html("border-style: " + csschoice_border_style[Math.round(Math.random() * 7)] + ";<br>border-radius: " + csschoice_size[Math.round(Math.random() * 19)] + ";<br>background-color: " + csschoice_color[Math.round(Math.random() * 19)] + ";<br>color: " + csschoice_color[Math.round(Math.random() * 19)] + ";<br>font-size: " + csschoice_size[Math.round(Math.random() * 19)] + ";<br>text-decoration: " + csschoice_text_decoration[Math.round(Math.random() * 3)] + ";<br>text-align: " + csschoice_text_align[Math.round(Math.random() * 2)] + ";<br>margin: " + csschoice_margin[Math.round(Math.random() * 19)] + ";<br>float: " + csschoice_float[Math.round(Math.random() * 1)] + ";<br>padding: " + csschoice_margin[Math.round(Math.random() * 19)] + ";<br>cursor: " + csschoice_cursor[Math.round(Math.random() * 16)] + ";<br>letter-spacing: " + csschoice_size[Math.round(Math.random() * 19)] + ";");
+        $('#4').html(goalText);
+        break;
+    default:
+        console.log("fail");
+}
+//$('#ec1').html("border-style: " + csschoice_border_style[Math.round(Math.random() * 7)] + ";<br>border-radius: " + csschoice_size[Math.round(Math.random() * 20)]/* + ";<br>background-color: " + csschoice_background_color[Math.round(Math.random() * 20)] + ";<br>color: " + csschoice_color[Math.round(Math.random() * 20)]*/ + ";<br>font-size: " + csschoice_size[Math.round(Math.random() * 20)] + ";<br>text-decoration: " + csschoice_text_decoration[Math.round(Math.random() * 3)] + ";<br>text-align: " + csschoice_text_align[Math.round(Math.random() * 2)] + ";<br>margin: " + csschoice_margin[Math.round(Math.random() * 20)] + ";<br>float: " + csschoice_float[Math.round(Math.random() * 1)] + ";<br>padding: " + csschoice_size[Math.round(Math.random() * 20)] + ";<br>cursor: " + csschoice_cursor[Math.round(Math.random() * 16)] + ";<br>height: " + csschoice_dims[Math.round(Math.random() * 20)] + ";<br>width: " + csschoice_dims[Math.round(Math.random() * 20)] + ";<br>letter-spacing: " + csschoice_size[Math.round(Math.random() * 20)] + ";");
+//console.log(csschoice_size[1] + "," + csschoice_size[2] + "," + csschoice_size[4] + "," + csschoice_size[3] + "," + csschoice_size[5]);
+
+};
+
+updateScore = function() {
+
+$('.footer').html("Correct: " + right + "/" + totalQuestions + " Wrong: " + wrong + " Score: " + score);
+$('.footer').css('margin-left','-' + $('.footer').css('width')/2);
+checkForWin();
+
 }
 
+checkForWin = function() {
+    if (right === totalQuestions || score === 1000) {
+        //won
+        alert("WIN!");
+        location.reload();//reload page for now
+        //show restart button here
+    } else if (right + wrong === totalQuestions) {
+        //lost
+        alert("LOSE! D:");
+        location.reload();//reload page for now
+        //show restart button here
+    } else {
+        return;
+    }
+}
+
+initGame = function() {
+    if (!hasStarted) {
+        console.log("Game started");
+        buildGoal();
+        setExampleContent();
+        setGoal();
+        hasStarted = true;
+        //console.log("initgame");
+    }
+};
+
+nextLevel = function(ans) {
+    if (hasStarted) {
+        if (parseInt(ans) === correctAnswer + 1) {
+            console.warn("Correct answer entered");
+            right++;
+            score += 10;
+        } else {
+            wrong++;
+        }
+        updateScore();
+        getVars();
+        buildGoal();
+        setExampleContent();
+        setGoal();
+    }
 };
